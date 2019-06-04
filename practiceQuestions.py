1a23d54b02eb39e0529fc720196a118e727390e7
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2019-06-04 08:39:43 -0500

    Finished 97

--- practiceQuestions.py
+++ practiceQuestions.py
@@ -953,7 +953,8 @@ Question 16
 #----------------------------------------#
 #Question 70
 
-        Please write a program which accepts basic mathematic expression from console and print the evaluation result.
+        Please write a program which accepts basic mathematic expression from 
+	console and print the evaluation result.
 
         #Example:
                 If the following string is given as input to the program:
@@ -968,307 +969,283 @@ Question 16
                 Use eval() to evaluate an expression.
 
 #----------------------------------------#
-Question:
+#Question 71
 
-Please write a binary search function which searches an item in a sorted list. The function should return the index of element to be searched in the list.
+	Please write a binary search function which searches an item in a sorted list. 
+	The function should return the index of element to be searched in the list.
 
-
-Hints:
-Use if/elif to deal with conditions.
-
-#----------------------------------------#
-Question:
-
-Please write a binary search function which searches an item in a sorted list. The function should return the index of element to be searched in the list.
-
-
-Hints:
-Use if/elif to deal with conditions.
+	#Hints:
+		Use if/elif to deal with conditions.
 
 #----------------------------------------#
-Question:
+#Question 72
 
-Please generate a random float where the value is between 10 and 100 using Python math module.
+	Generate a random float with a value between 10 and 100 using Python math module.
 
-
-
-Hints:
-Use random.random() to generate a random float in [0,1].
+	#Hints:
+		Use random.random() to generate a random float in [0,1].
 
 
 #----------------------------------------#
-Question:
+#Question 73
 
-Please generate a random float where the value is between 5 and 95 using Python math module.
+	Generate a random float with a value between 5 and 95 using Python math module.
 
-
-
-Hints:
-Use random.random() to generate a random float in [0,1].
+	#Hints:
+		Use random.random() to generate a random float in [0,1].
 
 #----------------------------------------#
-Question:
+#Question 74
 
-Please write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension.
+	Please write a program to output a random even number between 0 and 10 inclusive using 
+	random module and list comprehension.
 
-
-
-Hints:
-Use random.choice() to a random element from a list.
-
-#----------------------------------------#
-Question:
-
-Please write a program to output a random number, which is divisible by 5 and 7, between 0 and 10 inclusive using random module and list comprehension.
-
-
-
-Hints:
-Use random.choice() to a random element from a list.
+	#Hints:
+		Use random.choice() to a random element from a list.
 
 #----------------------------------------#
+#Question 75
 
-Question:
+	Please write a program to output a random number, which is divisible by 5 and 7, between 0 and 10
+	inclusive using random module and list comprehension.
 
-Please write a program to generate a list with 5 random numbers between 100 and 200 inclusive.
-
-
-
-Hints:
-Use random.sample() to generate a list of random values.
+	#Hints:
+		Use random.choice() to a random element from a list.
 
 #----------------------------------------#
-Question:
+#Question 76
 
-Please write a program to randomly generate a list with 5 even numbers between 100 and 200 inclusive.
+	Please write a program to generate a list with 5 random numbers between 100 and 200 inclusive.
 
-
-
-Hints:
-Use random.sample() to generate a list of random values.
+	#Hints:
+		Use random.sample() to generate a list of random values.
 
 #----------------------------------------#
-Question:
+#Question 77
 
-Please write a program to randomly generate a list with 5 numbers, which are divisible by 5 and 7 , between 1 and 1000 inclusive.
+	Please write a program to randomly generate a list with 5 even numbers between 100 and 200 inclusive.
 
-
-
-Hints:
-Use random.sample() to generate a list of random values.
-
+	#Hints:
+		Use random.sample() to generate a list of random values.
 
 #----------------------------------------#
+#Question 78
 
-Question:
+	Please write a program to randomly generate a list with 5 numbers, which are divisible by 5 and 7, 
+	between 1 and 1000 inclusive.
 
-Please write a program to randomly print a integer number between 7 and 15 inclusive.
-
-
-
-Hints:
-Use random.randrange() to a random integer in a given range.
+	#Hints:
+		Use random.sample() to generate a list of random values.
 
 #----------------------------------------#
+#Question 79
 
-Question:
+	Please write a program to randomly print a integer number between 7 and 15 inclusive.
 
-Please write a program to compress and decompress the string "hello world!hello world!hello world!hello world!".
-
-
-
-Hints:
-Use zlib.compress() and zlib.decompress() to compress and decompress a string.
+	#Hints:
+		Use random.randrange() to a random integer in a given range.
 
 #----------------------------------------#
-Question:
+#Question 80
 
-Please write a program to print the running time of execution of "1+1" for 100 times.
+	Please write a program to compress and decompress the string "hello world!hello world!hello world!hello world!".
 
-
-
-Hints:
-Use timeit() function to measure the running time.
+	#Hints:
+		Use zlib.compress() and zlib.decompress() to compress and decompress a string.
 
 #----------------------------------------#
-Question:
+#Question 81
 
-Please write a program to shuffle and print the list [3,6,7,8].
+	Please write a program to print the running time of execution of "1+1" for 100 times.
 
-
-
-Hints:
-Use shuffle() function to shuffle a list.
+	#Hints:
+		Use timeit() function to measure the running time.
 
 #----------------------------------------#
-Question:
+#Question 82
 
-Please write a program to shuffle and print the list [3,6,7,8].
+	Please write a program to shuffle and print the list [3,6,7,8].
 
-
-
-Hints:
-Use shuffle() function to shuffle a list.
+	#Hints:
+		Use shuffle() function to shuffle a list.
 
 #----------------------------------------#
-Question:
+#Question 83
 
-Please write a program to generate all sentences where subject is in ["I", "You"] and verb is in ["Play", "Love"] and the object is in ["Hockey","Football"].
+	Please write a program to shuffle and print the list [3,6,7,8].
 
-Hints:
-Use list[index] notation to get a element from a list.
-
-#----------------------------------------#
-Please write a program to print the list after removing delete even numbers in [5,6,77,45,22,12,24].
-
-Hints:
-Use list comprehension to delete a bunch of element from a list.
+	#Hints:
+		Use shuffle() function to shuffle a list.
 
 #----------------------------------------#
-Question:
+#Question 84
 
-By using list comprehension, please write a program to print the list after removing delete numbers which are divisible by 5 and 7 in [12,24,35,70,88,120,155].
+	Please write a program to generate all sentences where subject is in ["I", "You"] 
+	and verb is in ["Play", "Love"] and the object is in ["Hockey","Football"].
 
-Hints:
-Use list comprehension to delete a bunch of element from a list.
+	#Hints:
+		Use list[index] notation to get a element from a list.
 
 #----------------------------------------#
-Question:
+#Question 85
+					      
+	Please write a program to print the list after removing even numbers in 
+	[5,6,77,45,22,12,24].
 
-By using list comprehension, please write a program to print the list after removing the 0th, 2nd, 4th,6th numbers in [12,24,35,70,88,120,155].
+	#Hints:
+		Use list comprehension to delete a bunch of element from a list.
 
-Hints:
-Use list comprehension to delete a bunch of element from a list.
-Use enumerate() to get (index, value) tuple.
+#----------------------------------------#
+#Question 85
+
+	By using list comprehension, please write a program to print the list after
+	removing numbers which are divisible by 5 and 7 in [12,24,35,70,88,120,155].
+
+	#Hints:
+		Use list comprehension to delete a bunch of element from a list.
+
+#----------------------------------------#
+#Question 87
+
+	By using list comprehension, please write a program to print the list after 
+	removing the 0th, 2nd, 4th,6th numbers in [12,24,35,70,88,120,155].
+
+	#Hints:
+		Use list comprehension to delete a bunch of element from a list.
+		Use enumerate() to get (index, value) tuple.
 
 #----------------------------------------#
 
-Question:
+#Question 88
 
-By using list comprehension, please write a program generate a 3*5*8 3D array whose each element is 0.
+	By using list comprehension, please write a program generate a 3*5*8 3D array 
+	whose each element is 0.
 
-Hints:
-Use list comprehension to make an array.
-
-#----------------------------------------#
-Question:
-
-By using list comprehension, please write a program to print the list after removing the 0th,4th,5th numbers in [12,24,35,70,88,120,155].
-
-Hints:
-Use list comprehension to delete a bunch of element from a list.
-Use enumerate() to get (index, value) tuple.
+	#Hints:
+		Use list comprehension to make an array.
 
 #----------------------------------------#
+#Question 89
 
-Question:
+	By using list comprehension, please write a program to print the list after 
+	removing the 0th,4th,5th numbers in [12,24,35,70,88,120,155].
 
-By using list comprehension, please write a program to print the list after removing the value 24 in [12,24,35,24,88,120,155].
-
-Hints:
-Use list's remove method to delete a value.
-
-#----------------------------------------#
-Question:
-
-With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program to make a list whose elements are intersection of the above given lists.
-
-Hints:
-Use set() and "&=" to do set intersection operation.
+	#Hints:
+		Use list comprehension to delete a bunch of element from a list.
+		Use enumerate() to get (index, value) tuple.
 
 #----------------------------------------#
+#Question 90
 
-With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print this list after removing all duplicate values with original order reserved.
+	By using list comprehension, please write a program to print the list after 
+	removing the value 24 in [12,24,35,24,88,120,155].
 
-Hints:
-Use set() to store a number of values without duplicate.
-
-#----------------------------------------#
-Question:
-
-Define a class Person and its two child classes: Male and Female. All classes have a method "getGender" which can print "Male" for Male class and "Female" for Female class.
-
-Hints:
-Use Subclass(Parentclass) to define a child class.
+	#Hints:
+		Use list's remove method to delete a value.
 
 #----------------------------------------#
-Question:
+#Question 90
 
-Please write a program which count and print the numbers of each character in a string input by console.
+	With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], write a program 
+	to make a list whose elements are in both of the above given lists.
 
-Example:
-If the following string is given as input to the program:
-
-abcdefgabc
-
-Then, the output of the program should be:
-
-a,2
-c,2
-b,2
-e,1
-d,1
-g,1
-f,1
-
-Hints:
-Use dict to store key/value pairs.
-Use dict.get() method to lookup a key with default value.
+	#Hints:
+		Use set() and "&=" to do set intersection operation.
 
 #----------------------------------------#
+#Question 91
+					      
+	With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print this list
+	after removing all duplicate values with original order reserved.
 
-Question:
-
-Please write a program which accepts a string from console and print it in reverse order.
-
-Example:
-If the following string is given as input to the program:
-
-rise to vote sir
-
-Then, the output of the program should be:
-
-ris etov ot esir
-
-Hints:
-Use list[::-1] to iterate a list in a reverse order.
+	#Hints:
+		Use set() to store a number of values without duplicate.
 
 #----------------------------------------#
+#Question 92
 
-Question:
+	Define a class Person and its two child classes: Male and Female. All classes have 
+	a method "getGender" which can print "Male" for Male class and "Female" for Female class.
 
-Please write a program which accepts a string from console and print the characters that have even indexes.
-
-Example:
-If the following string is given as input to the program:
-
-H1e2l3l4o5w6o7r8l9d
-
-Then, the output of the program should be:
-
-Helloworld
-
-Hints:
-Use list[::2] to iterate a list by step 2.
+	#Hints:
+		Use Subclass(Parentclass) to define a child class.
 
 #----------------------------------------#
+#Question 93
 
+	Please write a program which count and print the numbers of each character in a string 
+	input by console.
 
-Question:
+	#Example:
+		If the following string is given as input to the program:
 
-Please write a program which prints all permutations of [1,2,3]
+			abcdefgabc
 
+		Then, the output of the program should be:
 
-Hints:
-Use itertools.permutations() to get permutations of list.
+			a,2
+			c,2
+			b,2
+			e,1
+			d,1
+			g,1
+			f,1
+
+	#Hints:
+		Use dict to store key/value pairs.
+		Use dict.get() method to lookup a key with default value.
 
 #----------------------------------------#
-Question:
+Question 94
 
-Write a program to solve a classic ancient Chinese puzzle: 
-We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have?
+	Please write a program which accepts a string from console and print it in reverse order.
 
-Hint:
-Use for loop to iterate all possible solutions.
+	#Example:
+		If the following string is given as input to the program:
+
+			rise to vote sir
+
+		Then, the output of the program should be:
+
+			ris etov ot esir
+
+	#Hints:
+		Use list[::-1] to iterate a list in a reverse order.
+
+#----------------------------------------#
+#Question 95
+
+	Please write a program which accepts a string from console and print the 
+	characters that have even indexes.
+
+	#Example:
+		If the following string is given as input to the program:
+
+			H1e2l3l4o5w6o7r8l9d
+
+		Then, the output of the program should be:
+
+			Helloworld
+
+	#Hints:
+		Use list[::2] to iterate a list by step 2.
+
+#----------------------------------------#
+#Question 96
+
+	Please write a program which prints all permutations of [1,2,3]
+
+	#Hints:
+		Use itertools.permutations() to get permutations of list.
+
+#----------------------------------------#
+#Question 97
+
+	Write a program to solve a classic ancient Chinese puzzle: 
+	We count 35 heads and 94 legs among the chickens and rabbits in a farm. 
+	How many rabbits and how many chickens do we have?
+
+	#Hint:
+		Use for loop to iterate all possible solutions.
 
 #----------------------------------------#
