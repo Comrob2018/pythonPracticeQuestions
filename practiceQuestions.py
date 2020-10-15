27f0c45903f728ee4b68ae1c2e32e97f34fe7a09
Comrob2018 <42179565+Comrob2018@users.noreply.github.com>
2020-10-15 14:11:24 -0500

    Update practiceQuestions.py

--- practiceQuestions.py
+++ practiceQuestions.py
@@ -28,7 +28,7 @@
 #----------------------------------------#
 #Question 3
 		
-	With a given integral number n, write a program to generate a dictionary that
+	With a given integer n, write a program to generate a dictionary that
 	contains (i, i*i) such that is an integral number between 1 and n (both included).
 	Then the program should print the dictionary.
 		
